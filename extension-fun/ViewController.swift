ad31efcaa97ba506e2fe73b866bf2c1053ac9c9d
vattanac <vattanacsim99@gmail.com>
2018-10-07 22:17:09 +0700

    Initial Commit

---
+++ extension-fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  extension-fun
+//
+//  Created by Vattanac on 10/7/18.
+//  Copyright © 2018 vattanac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
